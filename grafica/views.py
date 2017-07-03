f8febf5b9a62f1d365c3486d13d1e1af7deceec5
Simone Federici <s.federici@geniusbytes.com>
2017-07-03 19:19:24 +0200

    lazy import

--- grafica/views.py
+++ grafica/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.template.response import TemplateResponse
-
-from mezzanine.pages.models import RichTextPage
-
 
 def export(request):
+    from django.template.response import TemplateResponse
+    from mezzanine.pages.models import RichTextPage
     return TemplateResponse(request, "pages/export.html", dict(pages=RichTextPage.objects.all()))
